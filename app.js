1724e82fe7c03adcb9ee72d78a20dac164bc68da
mdaijaj <aijaj18@navgurukul.org>
2021-04-13 08:22:21 +0530

    add gitignoure file

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 const express = require('express')
 const app = express()
-const neatCsv = require('neat-csv');
-const fs = require('fs')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let user=require('./routes/user')
 app.use('/', user.router)
-
-
-const imageThumbnail = require('image-thumbnail');
-
-const data=async ()=>{
-
-imageThumbnail('resources/images/dog.jpg')
-    .then(thumbnail => { console.log(thumbnail) })
-    .catch(err => console.error(err));
-}
-data()
-
-
-
 
 var port =5000;
 app.listen(port, ()=>{
